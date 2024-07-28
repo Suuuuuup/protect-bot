351eb684bb412e35fe52090c83f7fd36bda64557
Suuuuuup <suuuuup@proton.me>
2024-07-28 17:35:59 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,22 @@
 @echo off
 color b
-echo Lancement du bot !
-python main.py
+title Démarrage du Bot Discord
+echo ===============================
+echo  Démarrage du Bot Discord
+echo ===============================
+echo.
 
+echo Installation des dependances...
+pip install -r requirements.txt
+
+echo.
+echo ===============================
+echo  Démarrage du Bot
+echo ===============================
+echo.
+
+
+python main.py
 pause
+
+
